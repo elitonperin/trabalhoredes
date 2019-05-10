625aa3a45e280467a888c0021dc2f82241b39c04
Eliton <elitonluiz93@gmail.com>
2019-05-10 12:19:10 -0400

    Mudando protocolo, antigo era TPC, agora UDP. Funcao de broadcast implementada.

--- leecher.py
+++ leecher.py
@@ -1,15 +1,31 @@
 import socket 
 
+class Leecher():
+    def __init__(self, args=None):
+        self.port = 7001
+        self.ip_broadcast = '192.168.100.255'
+        pass
+
+    def broadcast(self):
+        cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        cs.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        cs.sendto('This is a test'.encode(),
+                  (self.ip_broadcast, self.port))
+        print('Broadcast enviado')
+
+leecher = Leecher()
+leecher.broadcast()
+
 # ip = input('digite o ip de conexao: ') 
-ip = '127.0.0.1'
-port = 7001 
+ip = '192.168.100.20'
+port = 7001
 addr = ((ip,port)) 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-client_socket.connect(addr) 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mensagem = input("digite uma mensagem para enviar ao servidor: ")
 packet = bytearray(mensagem, 'utf8')
-client_socket.send(packet) 
+client_socket.sendto(packet, addr) 
 print ('mensagem enviada' )
-packet = client_socket.recv(1280)
+packet, addr = client_socket.recvfrom(1280)
 print('Mensagem recebida: ', packet.decode())
 client_socket.close()
--- seeder.py
+++ seeder.py
@@ -1,15 +1,54 @@
 import socket 
+import time
+import os
+import argparse
 
 class Seeder():
     def __init__(self, host='127.0.0.1', 
                  port=7001,
-                 users_listen=10 ):
-        self.host = host
-        self.port = port 
-        self.addr = (host, port) 
-        self.serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                 sharead_path='.',
+                 args=None):
+        if args.port is not None:
+            self.port=args.port
+        else:
+            self.port = port
+        if args.path is not None:
+            self.sharead_path=args.path
+        else:
+            self.sharead_path = sharead_path
+
+
+        if args.host is not None:
+            if args.host == 'find':
+                self.host = self.get_my_local_ip()
+            else:
+                self.host = args.host
+        else:
+            self.host = host 
+        
+        self.addr = (self.host, self.port)
+        print('Meu endereco: ', self.addr) 
+        self.serv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+        self.serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.serv_socket.bind(self.addr) 
-        self.serv_socket.listen(users_listen) 
+
+    def get_files_for_share(self):
+        caminhos = [os.path.join(self.sharead_path, nome) for nome in os.listdir(self.sharead_path)]
+        arquivos = [arq.split('\\')[-1] for arq in caminhos if os.path.isfile(arq)]
+        return arquivos
+
+    def get_my_local_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+
+        data_addr = s.getsockname()
+        print("IP local: ", data_addr[0])
+        s.close()
+        return data_addr[0]
+
+    def split_files(self):
+        pass
 
     def sequencial_transmission(self):
         pass
@@ -25,12 +64,13 @@ class Seeder():
 
         while var_exit:
             print ('aguardando conexao')
-            con, client = self.serv_socket.accept() 
-            print ('conectado com: ', str(client)) 
             print ("aguardando mensagem")
-            packet = con.recv(1280) 
+            packet, addr = self.serv_socket.recvfrom(1280) 
+            print ('recebido de: ', str(addr)) 
             print ("mensagem recebida: "+ packet.decode())
-            con.send(packet)
+            #dormir por 20 ms
+            time.sleep(.020)
+            self.serv_socket.sendto(packet, addr)
             if packet.decode() == 'exit':
                 var_exit = False
                 print("Fechando servidor")
@@ -38,6 +78,15 @@ class Seeder():
         self.serv_socket.close()
 
 if __name__ == "__main__":
-    server = Seeder()
+    parser = argparse.ArgumentParser(description="testing btpeer.py")
+    parser.add_argument("--port", type=int)
+    parser.add_argument("--host", type=str)
+    parser.add_argument("--path", type=str)
+    args = parser.parse_args()
+
+    server = Seeder(args=args)
+    
+    print(server.get_files_for_share())
     server.run_server()
+    
     exit()

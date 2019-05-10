6cf5a38b784eb5649b13547b459bb004f5dfb22b
Eliton <elitonluiz93@gmail.com>
2019-05-10 16:15:31 -0400

    Leitor de audio.

--- seeder.py
+++ seeder.py
@@ -2,6 +2,10 @@ import socket
 import time
 import os
 import argparse
+from pygame import mixer
+import pyaudio
+import wave
+import sys
 
 class Seeder():
     def __init__(self, host='127.0.0.1', 
@@ -9,8 +13,9 @@ class Seeder():
                  sharead_path='.',
                  args=None):
         self.max_data_legth = 1280
-        self.ext_files = ['mp3']
+        self.ext_files = ['wav']
         self.APP_KEY = 'APP_KEY'
+        self.header_size = 4
         
         if args.port is not None:
             self.port=args.port
@@ -55,8 +60,38 @@ class Seeder():
         s.close()
         return data_addr[0]
 
-    def split_files(self):
+    def split_files(self, name_file):
+        size = os.path.getsize(name_file)
+        size_data = size/(self.max_data_legth - self.header_size)
+        f = open(name_file, 'rb')
+        # data = f.read(int(size_data))
+        data = f.read()
+        self.play_audio(data)
+        f.close()
         pass
+
+    def play_audio(self, data):
+        
+        chunk = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 50000
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format = FORMAT,
+                        channels = CHANNELS,
+                        rate = RATE,
+                        input = True,
+                        output = True,
+                        frames_per_buffer = chunk)
+
+        for i in range(0, len(data), chunk):
+            stream.write(data[i:i+chunk])
+        stream.stop_stream()
+        stream.close()
+
+        p.terminate()
 
     def sequencial_transmission(self):
         pass
@@ -77,6 +112,7 @@ class Seeder():
             print ('recebido de: ', str(addr)) 
             print ("mensagem recebida: "+ packet.decode())
             #dormir por 20 ms
+            packet = self.get_only_music_files()
             time.sleep(.020)
             self.serv_socket.sendto(packet, addr)
             if packet.decode() == 'exit':
@@ -94,7 +130,9 @@ if __name__ == "__main__":
 
     server = Seeder(args=args)
     
-    print(server.get_only_music_files())
-    server.run_server()
+    # print(server.get_only_music_files())
+    # server.run_server()
+    server.split_files(server.get_only_music_files()[0])
+
     
     exit()
